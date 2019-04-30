5128867fd0aeaf236eb097b2ecfb9cceea47fed8
Emmanuel Martinez <emmartinez86@gmail.com>
2019-04-30 02:13:27 -0300

    Fix Deploy Issues

--- webpack.config.js
+++ webpack.config.js
@@ -6,16 +6,16 @@ const CleanWebpackPlugin = require("clean-webpack-plugin");
 
 module.exports = env => {
   const plugins = [
-    new ExtractTextPlugin("css/[name].css"),
+    new ExtractTextPlugin("css/[name].[hash].css"),
     new HtmlWebPackPlugin({
       template: "./src/index.html",
-      filename: "./index.html"
+      filename: "../index.html"
     })
   ]
 
   if (env.NODE_ENV === "production") {
     plugins.push(
-      new CleanWebpackPlugin(["dist"], { root: "/" })
+      new CleanWebpackPlugin(["dist"], { root: __dirname })
     )
   }
 
@@ -25,10 +25,10 @@ module.exports = env => {
       "iss-livelocation-ui": path.resolve(__dirname, "src/index.js")
     },
     output: {
-      path: path.resolve(__dirname, "./dist"),
+      path: path.resolve(__dirname, "dist"),
       filename: "js/[name].js",
-      publicPath: path.resolve(__dirname, "./dist") + "/",
-      chunkFilename: "js/[id].js"
+      publicPath: path.resolve(__dirname, "dist") + "/",
+      chunkFilename: "js/[id].[hash].js"
     },
     devServer: {
       port: 9000
@@ -66,7 +66,7 @@ module.exports = env => {
             options: {
               limit: 10000,
               fallback: "file-loader",
-              name: "images/[name].[ext]"
+              name: "images/[name].[hash].[ext]"
             }
           }
         },
