c585909af4ecacda66221df4031d0c3cb1c18d72
Emmanuel Martinez <emmartinez86@gmail.com>
2019-05-01 13:44:37 -0300

    Add new API to use https and fix Webpack Dev Build

--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -1,43 +1,61 @@
 const path = require("path");
+const HtmlWebPackPlugin = require("html-webpack-plugin");
 
-module.exports = {
-  entry: {
-    "iss-livelocation-ui": path.resolve(__dirname, "index.js")
-  },
-  output: {
-    path: path.resolve(__dirname, "dist"),
-    filename: "js/[name].js"
-  },
-  devServer: {
-    port: 9000
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(js|jsx)$/,
-        exclude: /(node_modules)/,
-        use: {
-          loader: "babel-loader",
-          options: {
-            presets: ["es2015", "react", "stage-2"]
+module.exports = env => {
+  const plugins = [
+    new HtmlWebPackPlugin({
+      template: "./src/index.html",
+      filename: "./index.html"
+    })
+  ]
+  return {
+    entry: {
+      "iss-livelocation-ui": path.resolve(__dirname, "./src/index.js")
+    },
+    output: {
+      path: path.resolve(__dirname, "dist"),
+      filename: "js/[name].js"
+    },
+    devServer: {
+      port: 9000
+    },
+    module: {
+      rules: [
+        {
+          test: /\.(js|jsx)$/,
+          exclude: /(node_modules)/,
+          use: {
+            loader: "babel-loader",
+            options: {
+              presets: ["es2015", "react", "stage-2"]
+            }
           }
-        }
-      },
-      {
-        test: /\.css$/,
-        use: ["style-loader", "css-loader"]
-      },
-      {
-        test: /\.(jpg|png|gif|svg)$/,
-        use: {
-          loader: "url-loader",
-          options: {
-            limit: 1000000,
-            fallback: "file-loader",
-            name: "images/[name].[hash].[ext]"
+        },
+        {
+          test: /\.css$/,
+          use: ["style-loader", "css-loader"]
+        },
+        {
+          test: /\.(jpg|png|gif|svg)$/,
+          use: {
+            loader: "url-loader",
+            options: {
+              limit: 1000000,
+              fallback: "file-loader",
+              name: "images/[name].[hash].[ext]"
+            }
           }
+        },
+        {
+          test: /\.html$/,
+          use: [
+            {
+              loader: "html-loader"
+            }
+          ]
         }
-      }
-    ]
+      ]
+    },
+    plugins
   }
 };
